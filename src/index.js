996ca74ad0b0f819ae98ea24ece8b5a972624f8b
devsfer1 <fernando.chaves16@hotmail.com>
2021-08-23 20:17:30 -0300

    feat: added initial files

---
+++ src/index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+//Tipos de parâmetros
+
+// Route Params => Identificar um recurso editar/deletar/buscar
+
+// Query Params => Paginação / Filtro
+
+// Body Params => Os objetos inserção/alteração (JSON)
+
+//localhost:porta/
+app.get("/courses", (request, response) => {
+  const query = request.query;
+  console.log(query);
+  return response.json([
+    "Curso 1",
+    "Curso 2",
+    "Curso 3"
+  ])
+});
+
+app.post("/courses", (request, response) => {
+  const body = request.body; 
+  console.log(body);
+  return response.json(["Curso 1",
+    "Curso 2",
+    "Curso 3",
+    "Curso 4"
+  ])
+});
+
+app.put("/courses/:id", (request, response) => {
+  const { id } = request.params;
+  console.log(id);
+  return response.json(["Curso 6",
+    "Curso 2",
+    "Curso 3",
+    "Curso 4"
+  ])
+})
+
+app.patch("/courses/:id", (request, response) => {
+  return response.json(["Curso 6",
+    "Curso 7",
+    "Curso 3",
+    "Curso 4"
+  ])
+})
+
+app.delete("/courses/:id", (request, response) => {
+  return response.json(["Curso 6",
+    "Curso 7",
+    "Curso 4"
+  ])
+})
+
+//localhost:porta
+app.listen(3333)
